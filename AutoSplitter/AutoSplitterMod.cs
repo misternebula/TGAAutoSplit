0d08e1809ecb812aa3a8157a925a1ebd9e80fd0e
_nebula <41904486+misternebula@users.noreply.github.com>
2023-03-28 15:58:27 +0100

    added debug mode thingy

--- AutoSplitter/AutoSplitterMod.cs
+++ AutoSplitter/AutoSplitterMod.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,6 +10,12 @@ using UnityEngine.SceneManagement;
 
 namespace AutoSplitter
 {
+	public class ConfigFile
+	{
+		[JsonProperty("debugMode")]
+		public bool debugMode;
+	}
+
 	internal class AutoSplitterMod : MonoBehaviour
 	{
 		bool _isRunActive = false;
@@ -21,110 +28,149 @@ namespace AutoSplitter
 		bool _splitForBackAlley;
 		bool _splitForEnd;
 
+		bool debugMode;
+
+		private static readonly GUIStyle guiGUIStyle = new GUIStyle();
+
+		public void Start()
+		{
+			guiGUIStyle.fontSize = 12;
+
+			var configFile = JsonConvert.DeserializeObject<ConfigFile>(File.ReadAllText($"{Application.dataPath}/Managed/config.json"));
+
+			debugMode = configFile.debugMode;
+		}
+
+		public void OnGUI()
+		{
+			if (Event.current.type != EventType.Repaint)
+			{
+				return;
+			}
+
+			guiGUIStyle.normal.textColor = Color.white;
+			GUI.contentColor = Color.white;
+
+			if (SceneManager.GetActiveScene().name == "Main Menu")
+			{
+				GUI.Label(new Rect(20, 10, 100, 100), "Autosplitter mod V1.2.0, by _nebula.", guiGUIStyle);
+			}
+
+			if (debugMode)
+			{
+				guiGUIStyle.fontSize = 30;
+				GUI.Label(new Rect(30, 30, 100, 100), "DEBUG MODE ENABLED", guiGUIStyle);
+				guiGUIStyle.fontSize = 12;
+			}
+		}
+
 		public void Update()
 		{
-			/*#region FNAF 4
-			if (FindObjectOfType<nBB_AI1>() != null)
+			if (debugMode)
 			{
-				FindObjectOfType<nBB_AI1>().flashed = true;
-				FindObjectOfType<nBB_AI1>().stade = 0;
-			}
+				#region FNAF 4
+				if (FindObjectOfType<nBB_AI1>() != null)
+				{
+					FindObjectOfType<nBB_AI1>().flashed = true;
+					FindObjectOfType<nBB_AI1>().stade = 0;
+				}
 
-			if (FindObjectOfType<nightmarionne_AI1>() != null)
-			{
-				FindObjectOfType<nightmarionne_AI1>().stade = 0;
-			}
+				if (FindObjectOfType<nightmarionne_AI1>() != null)
+				{
+					FindObjectOfType<nightmarionne_AI1>().stade = 0;
+				}
 
-			if (FindObjectOfType<nFredbear_AI1>() != null)
-			{
-				FindObjectOfType<nFredbear_AI1>().stade = 0;
-			}
+				if (FindObjectOfType<nFredbear_AI1>() != null)
+				{
+					FindObjectOfType<nFredbear_AI1>().stade = 0;
+				}
 
-			if (FindObjectOfType<Freddles_AI1>() != null)
-			{
-				FindObjectOfType<Freddles_AI1>().how_easy = 99999;
-				FindObjectOfType<Freddles_AI1>().playerOut = true;
-			}
-			#endregion
-			#region FNAF 2
-			if (FindObjectOfType<Mangle_AI1>() != null)
-			{
-				FindObjectOfType<Mangle_AI1>().enabled = false;
-			}
+				if (FindObjectOfType<Freddles_AI1>() != null)
+				{
+					FindObjectOfType<Freddles_AI1>().how_easy = 99999;
+					FindObjectOfType<Freddles_AI1>().playerOut = true;
+				}
+				#endregion
+				#region FNAF 2
+				if (FindObjectOfType<Mangle_AI1>() != null)
+				{
+					FindObjectOfType<Mangle_AI1>().enabled = false;
+				}
 
-			if (FindObjectOfType<Mangle_AI2>() != null)
-			{
-				FindObjectOfType<Mangle_AI2>().enabled = false;
-			}
+				if (FindObjectOfType<Mangle_AI2>() != null)
+				{
+					FindObjectOfType<Mangle_AI2>().enabled = false;
+				}
 
-			if (FindObjectOfType<ToyChica_AI1>() != null)
-			{
-				FindObjectOfType<ToyChica_AI1>().enabled = false;
-			}
+				if (FindObjectOfType<ToyChica_AI1>() != null)
+				{
+					FindObjectOfType<ToyChica_AI1>().enabled = false;
+				}
 
-			if (FindObjectOfType<WitheredFreddy_AI1>() != null)
-			{
-				FindObjectOfType<WitheredFreddy_AI1>().enabled = false;
+				if (FindObjectOfType<WitheredFreddy_AI1>() != null)
+				{
+					FindObjectOfType<WitheredFreddy_AI1>().enabled = false;
+				}
+				#endregion
+				#region FNAF3
+				if (FindObjectOfType<Dreadbear_AI1>() != null)
+				{
+					FindObjectOfType<Dreadbear_AI1>().enabled = false;
+					FindObjectOfType<Dreadbear_AI1>().stade = 1;
+				}
+				if (FindObjectOfType<Springtrap_AI1>() != null)
+				{
+					FindObjectOfType<Springtrap_AI1>().enabled = false;
+					FindObjectOfType<Springtrap_AI1>().stade = 0;
+				}
+				if (FindObjectOfType<PhantomFoxy_AI1>() != null)
+				{
+					FindObjectOfType<PhantomFoxy_AI1>().enabled = false;
+					FindObjectOfType<PhantomFoxy_AI1>().stade = 0;
+				}
+				if (FindObjectOfType<PhantomBalloonBoy_AI1>() != null)
+				{
+					FindObjectOfType<PhantomBalloonBoy_AI1>().enabled = false;
+				}
+				#endregion
+				#region FNAF SL
+				if (FindObjectOfType<FuntimeFoxy_AI1>() != null)
+				{
+					FindObjectOfType<FuntimeFoxy_AI1>().deactivated = true;
+				}
+				if (FindObjectOfType<FuntimeFreddy_AI1>() != null)
+				{
+					FindObjectOfType<FuntimeFreddy_AI1>().deactivated = true;
+				}
+				if (FindObjectOfType<Ballora_AI1>() != null)
+				{
+					FindObjectOfType<Ballora_AI1>().deactivated = true;
+				}
+				#endregion
+				#region ENNARD
+				if (FindObjectOfType<Ennard_AI>() != null)
+				{
+					FindObjectOfType<Ennard_AI>().enabled = false;
+				}
+				#endregion
+				#region BACK ALLEY
+				if (FindObjectOfType<Afton_AI>() != null)
+				{
+					FindObjectOfType<Afton_AI>().enabled = false;
+					FindObjectOfType<Afton_AI>().stade = 0;
+				}
+				if (FindObjectOfType<ScrapBaby_AI>() != null)
+				{
+					FindObjectOfType<ScrapBaby_AI>().enabled = false;
+					FindObjectOfType<ScrapBaby_AI>().stade = 0;
+				}
+				if (FindObjectOfType<MoltenFreddy_AI>() != null)
+				{
+					FindObjectOfType<MoltenFreddy_AI>().enabled = false;
+					FindObjectOfType<MoltenFreddy_AI>().stade = 0;
+				}
+				#endregion
 			}
-			#endregion
-			#region FNAF3
-			if (FindObjectOfType<Dreadbear_AI1>() != null)
-			{
-				FindObjectOfType<Dreadbear_AI1>().enabled = false;
-				FindObjectOfType<Dreadbear_AI1>().stade = 1;
-			}
-			if (FindObjectOfType<Springtrap_AI1>() != null)
-			{
-				FindObjectOfType<Springtrap_AI1>().enabled = false;
-				FindObjectOfType<Springtrap_AI1>().stade = 0;
-			}
-			if (FindObjectOfType<PhantomFoxy_AI1>() != null)
-			{
-				FindObjectOfType<PhantomFoxy_AI1>().enabled = false;
-				FindObjectOfType<PhantomFoxy_AI1>().stade = 0;
-			}
-			if (FindObjectOfType<PhantomBalloonBoy_AI1>() != null)
-			{
-				FindObjectOfType<PhantomBalloonBoy_AI1>().enabled = false;
-			}
-			#endregion
-			#region FNAF SL
-			if (FindObjectOfType<FuntimeFoxy_AI1>() != null)
-			{
-				FindObjectOfType<FuntimeFoxy_AI1>().deactivated = true;
-			}
-			if (FindObjectOfType<FuntimeFreddy_AI1>() != null)
-			{
-				FindObjectOfType<FuntimeFreddy_AI1>().deactivated = true;
-			}
-			if (FindObjectOfType<Ballora_AI1>() != null)
-			{
-				FindObjectOfType<Ballora_AI1>().deactivated = true;
-			}
-			#endregion
-			#region ENNARD
-			if (FindObjectOfType<Ennard_AI>() != null)
-			{
-				FindObjectOfType<Ennard_AI>().enabled = false;
-			}
-			#endregion
-			#region BACK ALLEY
-			if (FindObjectOfType<Afton_AI>() != null)
-			{
-				FindObjectOfType<Afton_AI>().enabled = false;
-				FindObjectOfType<Afton_AI>().stade = 0;
-			}
-			if (FindObjectOfType<ScrapBaby_AI>() != null)
-			{
-				FindObjectOfType<ScrapBaby_AI>().enabled = false;
-				FindObjectOfType<ScrapBaby_AI>().stade = 0;
-			}
-			if (FindObjectOfType<MoltenFreddy_AI>() != null)
-			{
-				FindObjectOfType<MoltenFreddy_AI>().enabled = false;
-				FindObjectOfType<MoltenFreddy_AI>().stade = 0;
-			}
-			#endregion*/
 
 			if (SceneManager.GetActiveScene().name == "Main Menu")
 			{
